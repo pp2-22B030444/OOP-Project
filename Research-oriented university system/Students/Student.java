e80411f4c34418e15889f685b88faf292aab3e26
Zhasmin Suleimenova <122953922+pp2-22B030444@users.noreply.github.com>
2023-12-13 23:26:26 +0600

    Update Student.java

--- Research-oriented university system/Students/Student.java
+++ Research-oriented university system/Students/Student.java
@@ -3,8 +3,10 @@ package Students ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -13,49 +15,35 @@ import javax.annotation.processing.SupportedSourceVersion;
 import Department.Course;
 import Department.Teacher;
 import Employee.Data;
+import Employee.Language;
 import Employee.User;
 
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
 
 public class Student extends User{
 	
 	private String id;
-	public String fullName;
 	public School school;
-	public Transcript transcript;
-	public Marks mark;
 	public int yearOfStudy;	
 	public GraduateStudent graduateStudent;
 	public Vector<Course> registeredCourses;
-
-//	public Map<Course, List<Marks>> transcripts;
-//	
-//	/**
-//	 * <!-- begin-user-doc -->
-//	 * <!--  end-user-doc  -->
-//	 * @generated
-//	 * @ordered
-//	 */
-//	
+	public Map<Course, Marks> marksMap;
 	public Map<Teacher, Integer> teacherRatings;
 	public Student(){
 		
 	}
-
-	public Student(String id, String fullName, School school, int yearOfStudy, GraduateStudent graduateStudent,
-			Vector<Course> registeredCourses,Map<Teacher, Integer> teacherRatings) {
+	public Student(String name, String surname, String birthDate, String phoneNumber, String login, String password) {
+		super(name, surname, birthDate, phoneNumber, login, password);
+		// TODO Auto-generated constructor stub
+	}
+	public Student(String id, School school, int yearOfStudy, GraduateStudent graduateStudent,
+			Vector<Course> registeredCourses,Map<Course, Marks> marksMap,Map<Teacher, Integer> teacherRatings) {
 		this();
 		this.id = id;
-		this.fullName = fullName;
 		this.school = school;
 		this.yearOfStudy = yearOfStudy;
 		this.graduateStudent = graduateStudent;
 		this.registeredCourses = registeredCourses;
+		this.marksMap=marksMap;
 		this.teacherRatings = teacherRatings;
 	}
 	public void viewCourses() {
@@ -76,102 +64,106 @@ public class Student extends User{
 		
 	}
 	public void viewTranscript() {
-		// TODO implement me
-		for (Course course : registeredCourses) {
-			Transcript t = new Transcript(course, mark);	
-        	System.out.println(t.showTranscript());
-	    }
-	}
+        // TODO implement me
+        for (Course course : registeredCourses) {
+            Marks marksForCourse = getMarksForCourse(course);
+            Transcript t = new Transcript(course, marksForCourse);
+            System.out.println(t.showTranscript());
+        }
+    }
 
-	public void viewMarks() {
-		// TODO implement me
-		for (Course course : registeredCourses) {
-	        // Assuming each course has an associated Marks instance
-			Marks m = new Marks(course, mark.getFirstAtt(), mark.getSecountAtt(), mark.getFinalGrade());
-	        System.out.println(m.showMark());
-	    }
-	}
+    public void viewMarks() {
+        // TODO implement me
+        for (Course course : registeredCourses) {
+            Marks marksForCourse = getMarksForCourse(course);
+            System.out.println(marksForCourse.showMark());
+        }
+    }
 
-	public void viewInfoAboutTeacher() {
-		// TODO implement me	
+    private Marks getMarksForCourse(Course course) {
+        return marksMap.get(course);
+    }
+
+
+	public void viewInfoAboutTeacher(Data data) {
+		// TODO implement me
+		for(Teacher t : data.teachers) {
+			System.out.println(t);
+		}
 	}
 
 	public void rateTeachers(Teacher teacher, int Rate ) {
 		// TODO implement me	
 	}
 	public void exit() {
-	    System.out.println("Bye bye");
-	}
+        System.out.println("Bye bye");
+    }
+    public void joinToOrganization(Student student, StudentOrganization o) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Welcome!");
 
-	public void joinToOrganization(Student student, StudentOrganization o) {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    System.out.println("Welcome!");
-	    try {
-	        menu: while (true) {
-	            System.out.println("Which organization do you want to join?\n1. OSIT\n2. BSL\n3. K2L\n4. Exit");
-	            int choice = Integer.parseInt(br.readLine());
-	            switch (choice) {
-	                case 1:
-	                    o.addMember(student, NameOrganization.OSIT);
-	                    break;
-	                case 2:
-	                    o.addMember(student, NameOrganization.BCL);
-	                    break;
-	                case 3:
-	                    o.addMember(student, NameOrganization.K2L);
-	                    break;
-	                case 4:
-	                    break menu;
-	                default:
-	                    System.out.println("Invalid choice. Please choose again.");
-	            }
-	        }
-	    } catch (IOException | NumberFormatException e) {
-	        System.out.println("Something bad happened! Saving resources");
-	        exit();
-	    } finally {
-	        try {
-	            br.close();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	}
+            menu: while (true) {
+                int choice = -1;
 
-//	public void joinToOrganization(Student student,StudentOrganization o) {
-//		System.out.println("Please select organization:");
-//	System.out.println("1. OSIT");
-//    System.out.println("2. BSL");
-//    System.out.println("3. K2L");
-//
-//    Scanner scanner = new Scanner(System.in);
-//    int choice = scanner.nextInt();
-//
-//    switch (choice) {
-//        case 1:
-//            o.addMember(student, NameOrganization.OSIT);
-//            break;
-//        case 2:
-//        	o.addMember(student, NameOrganization.BCL);
-//            break;
-//        case 3:
-//        	o.addMember(student, NameOrganization.K2L);
-//            break;
-//        default:
-//            System.out.println("Invalid choice.");
-//    }
-//    scanner.close();
-//    o.serializeOrganization();
-//    }	
-	public void viewTeacherInfo(Data data) {
-		// TODO implement me	
-		for(Teacher t : data.teachers) {
-			System.out.println(t);
-		}
+                while (choice < 1 || choice > 4) {
+                    try {
+                        System.out.println("Which organization do you want to join?\n1. OSIT\n2. BSL\n3. K2L\n4. Exit");
+                        choice = Integer.parseInt(br.readLine());
+
+                        if (choice < 1 || choice > 4) {
+                            System.out.println("Invalid choice. Please choose again.");
+                        }
+                    } catch (IOException | NumberFormatException e) {
+                        System.out.println("Invalid input. Please enter a number.");
+                    }
+                }
+
+                switch (choice) {
+                    case 1:
+                        o.addMember(student, NameOrganization.OSIT);
+                        break;
+                    case 2:
+                        o.addMember(student, NameOrganization.BCL);
+                        break;
+                    case 3:
+                        o.addMember(student, NameOrganization.K2L);
+                        break;
+                    case 4:
+                        exit();
+                        break menu;
+                    default:
+                        System.out.println("Invalid choice. Please choose again.");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Something bad happened! Saving resources");
+            exit();
+        }
+    }
+    
+	@Override
+	public int hashCode() {
+		return Objects.hash( graduateStudent, id, marksMap, registeredCourses, school, teacherRatings,
+				yearOfStudy);
 	}
 	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return  graduateStudent == other.graduateStudent
+				&& Objects.equals(id, other.id) && Objects.equals(marksMap, other.marksMap)
+				&& Objects.equals(registeredCourses, other.registeredCourses) && school == other.school
+				&& Objects.equals(teacherRatings, other.teacherRatings) && yearOfStudy == other.yearOfStudy;
+	}
+
+	@Override
 	public String toString() {
-		return "Student [id=" + id + ", fullName=" + fullName + ", school=" + school + ", yearOfStudy=" + yearOfStudy
+		return "Student [id=" + id +super.toString() + ", school=" + school + ", yearOfStudy=" + yearOfStudy
 				+ ", graduateStudent=" + graduateStudent +"]";
 	}
 	
