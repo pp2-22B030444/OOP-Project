3d9831ed571630e7242c9700a969d56fea02bbc2
Zhasmin Suleimenova <122953922+pp2-22B030444@users.noreply.github.com>
2023-12-14 00:34:54 +0600

    Update TechSupportOrder.java

--- Research-oriented university system/Employee/TechSupportOrder.java
+++ Research-oriented university system/Employee/TechSupportOrder.java
@@ -1,4 +1,4 @@
-package Employee ;
+=package Employee ;
 
 
 
@@ -18,7 +18,7 @@ public class TechSupportOrder
 	}
 
 	
-	public void getDescription() {
+	public String getDescription() {
 		return "TechSupportOrder [description=" + description + ", accepted=" + accepted + ", typeOfRequest="
 				+ typeOfRequest + "]";	
 	}
@@ -26,6 +26,26 @@ public class TechSupportOrder
 	public boolean isAccepted() {
 		return true;	
 	}
+    
+	public TypeOfRequest getTypeOfRequest() {
+		return typeOfRequest;
+	}
+
+
+	public void setTypeOfRequest(TypeOfRequest typeOfRequest) {
+		this.typeOfRequest = typeOfRequest;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public void setAccepted(boolean accepted) {
+		this.accepted = accepted;
+	}
+
 
 	@Override
 	public String toString() {
@@ -34,5 +54,3 @@ public class TechSupportOrder
 	}
 	
 }
-
-
