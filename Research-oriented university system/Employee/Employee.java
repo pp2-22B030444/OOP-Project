7c73825abbb65e79340de6c57d4f0474ccaf6846
Kumar Oskenbay <123387665+pp2-22B03495@users.noreply.github.com>
2023-12-13 22:29:37 +0600

    Update Employee.java

--- Research-oriented university system/Employee/Employee.java
+++ Research-oriented university system/Employee/Employee.java
@@ -1,38 +1,28 @@
-<<<<<<< HEAD
-/*НЕ ЗАКОНЧЕНА!!!*/
-
 package Employee;
 
 public abstract class Employee extends User implements Message{
 	
 	private String id;
-	public String fullName;
 	
 	public Employee(){
 		
 	}
-
-	public Employee(String id, String fullName,String userName, int password, Language language) {
-		super(userName, password, language);
+	
+public Employee(String name, String surname, String birthDate, String phoneNumber, String userName, String password,
+			Language language, String id) {
+		super(name, surname, birthDate, phoneNumber, userName, password, language);
 		this.id = id;
-		this.fullName = fullName;
 	}
 
+	
 	public String getId() {
-            return id;
+        return id;
     }
 
     public void setId(String id) {
         this.id = id;
     }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
 	
     public void sendMessage(String message) {
         System.out.println("Sending message: " + message);
@@ -49,63 +39,11 @@ public abstract class Employee extends User implements Message{
         /*Логика нужна*/
     }
 
-
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", fullName=" + fullName + "]";
+		return "Employee [id=" + id + ", getPassword()=" + getPassword() + ", getUserName()=" + getUserName()
+				+ ", getName()=" + getName() + ", getSurname()=" + getSurname() + ", getBirthDate()=" + getBirthDate()
+				+ ", getPhoneNumber()=" + getPhoneNumber() + ", chooseLanguage()=" + chooseLanguage() + "]";
 	}
 
-
-
-	
-	
 }
-=======
-
-/*НЕ ЗАКОНЧЕНА!!!*/ 
- 
-package Employee; 
-import java.io.*;
-
-public abstract class Employee extends User implements Message, Serializable{ 
-  
- private String id; 
-  
- public Employee(){ 
-   
- } 
- 
-  public Employee(String name, String surname, String birthDate, String phoneNumber, String login, String password) { 
-         super(name, surname, birthDate, phoneNumber, login, password); 
-          this.id = id;
-     } 
- 
-  
-  
-    public void sendMessage(String message) { 
-        System.out.println("Sending message: " + message); 
-        /*Логика нужна*/ 
-    } 
- 
-    public void makeRequest(String request) { 
-        System.out.println("Making request: " + request); 
-        /*Логика нужна*/ 
-    } 
- 
-    public void makeOrder(String order) { 
-        System.out.println("Making order: " + order); 
-        /*Логика нужна*/ 
-    } 
- 
- 
- @Override 
- public String toString() { 
-  return "Employee [id=" + id + ", fullName=" + fullName + "]"; 
- } 
- 
- 
- 
-  
-  
-}
->>>>>>> 9427334b2c9cd4b841d588820d7f6fd6ea69447a
