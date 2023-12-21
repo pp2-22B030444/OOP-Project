5254cfe54da4f3926a6e10c1f44fa8a5105eb0c0
Kumar Oskenbay <123387665+pp2-22B03495@users.noreply.github.com>
2023-12-21 23:50:38 +0600

    Update NewsPublisher.java

--- Research-oriented university system/Employee/NewsPublisher.java
+++ Research-oriented university system/Employee/NewsPublisher.java
@@ -3,9 +3,12 @@ package Employee;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Vector;
 
 public class NewsPublisher {
     private List<NewsObserver> observers;
+	public Vector<News> news;
+	
 
     public NewsPublisher() {
         this.observers = new ArrayList<>();
@@ -33,12 +36,21 @@ public class NewsPublisher {
         News news = new News(topic, text, date);
         notifyObservers(news);
     }
-
+    public void removeNews(News oldNews) {
+        int index = news.indexOf(oldNews);
+        if (index != -1) {
+            news.remove(index);
+            notifyObservers(oldNews);
+        }
+    }
+    
+    
     @Override
     public String toString() {
         return "NewsPublisher [observers=" + observers + "]";
     }
 
+    
+
+	
 }
-
-
