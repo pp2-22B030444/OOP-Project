6ba581a66cfd6166cb237c26c26cdea92a140181
aluuwwa5 <122877508+aluuwwa5@users.noreply.github.com>
2023-12-24 14:04:46 +0600

    Create  CreditsExceeded.java

---
+++ Research-oriented university system/Students/CreditsExceeded.java
@@ -0,0 +1,11 @@
+public class CreditsExceeded extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6605730744576429353L;
+
+	public CreditsExceeded(String message){
+        super(message);
+    }
+
+}
